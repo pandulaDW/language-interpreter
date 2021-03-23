7538eb452b7fd73f496d46fe49160948d00b4379
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-03-23 16:12:17 +0530

    Added string methods to statments

--- ast/ast.go
+++ ast/ast.go
@@ -1,12 +1,14 @@
 package ast
 
 import (
+	"bytes"
 	"github.com/pandulaDW/language-interpreter/tokens"
 )
 
 // Interfaces ----------------------------------
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -31,6 +33,16 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // Identifier Statement ------------------------
 type Identifier struct {
 	Token tokens.Token // the tokens.IDENT token
@@ -41,6 +53,10 @@ func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
 
 // LetStatement --------------------------------
@@ -58,6 +74,21 @@ func (l LetStatement) statementNode() {
 	panic("implement me")
 }
 
+func (l LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(l.TokenLiteral() + " ")
+	out.WriteString(l.Name.String())
+	out.WriteString(" = ")
+
+	if l.Value != nil {
+		out.WriteString(l.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
 // ReturnStatement ------------------------------
 type ReturnStatement struct {
 	Token       tokens.Token // The tokens.RETURN token
@@ -68,4 +99,35 @@ func (r ReturnStatement) statementNode() {}
 
 func (r ReturnStatement) TokenLiteral() string {
 	return r.Token.Literal
+}
+
+func (r ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(r.TokenLiteral() + " ")
+
+	if r.ReturnValue != nil {
+		out.WriteString(r.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+//ExpressionStatement -----------------------------
+type ExpressionStatement struct {
+	Token      tokens.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
